cf220414e4a063cca1d4dac5dd876e3f4de0904b
jimkueneman@yahoo.com <jimkueneman@yahoo.com@560af22c-e065-5297-5dba-4ba36a0b89cb>
2012-07-09 01:16:51 +0000

    July 8, 2012
      - Fixed bug in CDI Editor Tool

--- Tools/ACDI_Editor/UnitMainUnit.pas
+++ Tools/ACDI_Editor/UnitMainUnit.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ComCtrls, ExtCtrls;
+  Dialogs, StdCtrls, ComCtrls, ExtCtrls, StrUtils;
 
 const
   MAX_MFG_CHARS = 64;
@@ -12,27 +12,58 @@ const
   MAX_USER_DESC_CHARS = 40;
 
 type
+  TCDIStringInfo = record
+    Offset: Word;   // Offset in the Byte Array where the string starts
+    Length: Word;   // Length of the CDI String
+  end;
+  PCDIStringInfo = ^TCDIStringInfo;
+
+const
+  MAP_DELIMITER_COUNT = 4;
+  MAP_DELIMITER: array[0..MAP_DELIMITER_COUNT-1] of AnsiString = (
+    '<manufacturer>',                                                           // LOWERCASE
+    '<model>',
+    '<hardwareversion>',
+    '<softwareversion>'
+  );
+
+MAP_DELIMITER_TAIL: array[0..MAP_DELIMITER_COUNT-1] of AnsiString = (
+    '</manufacturer>',                                                          // LOWERCASE
+    '</model>',
+    '</hardwareversion>',
+    '</softwareversion>'
+  );
+
+type
+    TCDIMap = array[0..MAP_DELIMITER_COUNT-1] of TCDIStringInfo;
+
+type
   TForm1 = class(TForm)
-    EditMfg: TEdit;
-    EditName: TEdit;
-    EditHWVersion: TEdit;
-    EditSWVersion: TEdit;
-    EditUserDefinedName: TEdit;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
+    LabelMfgInfoVer: TLabel;
+    LabelUserInfoVersion: TLabel;
+    EditMfg: TEdit;
+    EditName: TEdit;
+    EditHWVersion: TEdit;
+    EditSWVersion: TEdit;
+    EditUserDefinedName: TEdit;
     EditUserDefinedDesc: TEdit;
     RichEdit1: TRichEdit;
     ButtonGenerateCode: TButton;
     ButtonDecompile: TButton;
-    LabelMfgInfoVer: TLabel;
     EditMfgInfoVer: TEdit;
-    LabelUserInfoVersion: TLabel;
     EditUserInfoVer: TEdit;
     GroupBox1: TGroupBox;
+    Bevel1: TBevel;
+    Bevel2: TBevel;
     LabelMfgLen: TLabel;
     LabelNodeNameLen: TLabel;
     LabelHardwareLen: TLabel;
@@ -48,9 +79,22 @@ type
     Label19: TLabel;
     LabelMfgSubTotal: TLabel;
     Label21: TLabel;
-    Bevel1: TBevel;
-    Bevel2: TBevel;
     CheckBoxACDI: TCheckBox;
+    EditXMLFile: TEdit;
+    Button1: TButton;
+    Label7: TLabel;
+    Button2: TButton;
+    OpenDialog1: TOpenDialog;
+    MemoCDI: TMemo;
+    Label8: TLabel;
+    Label9: TLabel;
+    EditCDIArray: TEdit;
+    EditCDIMap: TEdit;
+    Label10: TLabel;
+    EditMaxCDIArray: TEdit;
+    CheckBoxVirtualNodeCDI: TCheckBox;
+    Label11: TLabel;
+    LabelMaxMfgStrLen: TLabel;
     procedure EditMfgChange(Sender: TObject);
     procedure EditNameChange(Sender: TObject);
     procedure EditHWVersionChange(Sender: TObject);
@@ -61,6 +105,8 @@ type
     procedure ButtonDecompileClick(Sender: TObject);
     procedure EditMfgInfoVerKeyPress(Sender: TObject; var Key: Char);
     procedure EditMfgInfoVerExit(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -70,6 +116,7 @@ type
     function DecompileString(Str: string): string;
     function DecompileVersionString(Str: string): string;
     procedure UpdateUI;
+    function IdentifyMapItem(Delimiter, DelimiterTail: AnsiString; TestStr: AnsiString; MapInfo: PCDIStringInfo; ByteTotal: Integer): Boolean;
   end;
 
 var
@@ -313,6 +360,179 @@ begin
     LabelMaxDesc.Font.Color := clWindowText;
     LabelMaxDesc.Font.Style := []
   end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+  begin
+    EditXMLFile.Text := OpenDialog1.FileName;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  XML: TFileStream;
+  Str: PANSIChar;
+  i, j, AsciiArrayCount, Offset, Index, ExtraLines: Integer;
+  Strings, ASCII: TStringList;
+  ASCII_Line: AnsiString;
+  Map: TCDIMap;
+  ArrayIdentifier, MaxArrayIdentifier, MapIdentifier: AnsiString;
+begin
+  MemoCDI.Lines.BeginUpdate;
+  try
+    MemoCDI.Clear;
+    XML := TFileStream.Create(EditXMLFile.Text, fmOpenRead or fmShareDenyWrite);
+    try
+      // Read in the XML file from disk
+      Str := StrAlloc(XML.Size);
+      XML.ReadBuffer(Str^, XML.Size);
+
+      Strings := TStringList.Create;
+      ASCII := TStringList.Create;
+      try
+        AsciiArrayCount := 0;
+        Strings.Text := Str;
+        ExtraLines := -1;
+
+        // Strip off any white space, (space, tabs, etc), extra lines, garbage after </cdi>, etc
+        for i := 0 to Strings.Count - 1 do
+        begin
+          Strings[i] := Trim(Strings[i]);
+          Index := Pos('</cdi>', LowerCase(Strings[i]));
+          if Index > 0 then
+          begin
+            Inc(Index, 5);
+            ASCII_Line := Strings[i];
+            SetLength(ASCII_Line, Index);
+            Strings[i] := ASCII_Line;
+            ExtraLines := Strings.Count - 1 - i;
+          end;
+        end;
+        for i := 0 to ExtraLines - 1 do
+          Strings.Delete(Strings.Count-1);
+
+
+        // Create the ASCII array for the XML characters
+        for i := 0 to Strings.Count - 1 do
+        begin
+          if Length(Strings[i]) > 0 then
+          begin
+            ASCII_Line := '';
+            for j := 1 to Length(Strings[i]) do
+            begin
+              ASCII_Line := ASCII_Line + '$' + IntToHex( Ord(Strings[i][j]), 2) + ', ';
+              Inc(AsciiArrayCount)
+            end;
+            // Strip off the ',' on the last line
+            if i = Strings.Count - 1 then
+              SetLength(ASCII_Line, Length(ASCII_Line) - 2);
+
+            ASCII_Line := '    ' + ASCII_Line + '   // ' + Strings[i];
+            ASCII.Add(ASCII_Line);
+          end
+        end;
+
+        // Build the mPascal Code
+        if CheckBoxVirtualNodeCDI.Checked then
+        begin
+          ArrayIdentifier := EditCDIArray.Text + '_VNODE';
+          MaxArrayIdentifier := EditMaxCDIArray.Text + '_VNODE';
+          MapIdentifier := EditCDIMap.Text + '_VNODE';
+        end else
+        begin
+          ArrayIdentifier := EditCDIArray.Text;
+          MaxArrayIdentifier := EditMaxCDIArray.Text;
+          MapIdentifier := EditCDIMap.Text;
+        end;
+
+        ASCII.Insert(0, 'const');
+        ASCII.Insert(1, '  ' + MaxArrayIdentifier + ' = ' + IntToStr(AsciiArrayCount) + ';' );
+        ASCII.Insert(2, '  ' + ArrayIdentifier + ': array[0..' + MaxArrayIdentifier + '-1] of byte = (' );
+
+        // Create a lookup Map of the CDI constant data is within the strings (Manufacturer Name, Manufacturer Model, Manufacturer Hardware Version, Manufacturer Sofware Version, etc)
+        for j := 0 to MAP_DELIMITER_COUNT - 1 do
+        begin
+          Offset := 0;
+          i := 0;
+          while (i < Strings.Count) do
+          begin
+            IdentifyMapItem(MAP_DELIMITER[j], MAP_DELIMITER_TAIL[j], Strings[i], @Map[j], Offset);
+            Offset := Offset + Length(Strings[i]);
+            Inc(i);
+          end;
+        end;
+        ASCII.Add('  );');
+        ASCII.Add('');
+        ASCII.Add('type');
+        ASCII.Add('  TCDIStringInfo = record');
+        ASCII.Add('    Offset: Word;   // Offset in the Byte Array where the string starts');
+        ASCII.Add('    Length: Word;   // Length of the CDI String');
+        ASCII.Add('  end;');
+        ASCII.Add('  PCDIStringInfo = ^TCDIStringInfo;');
+        ASCII.Add('');
+        ASCII.Add('type');
+        ASCII.Add('  TCDIStringMap = record');
+        ASCII.Add('    Manufacturer         : TCDIStringInfo;');
+        ASCII.Add('    ManufacturerModel    : TCDIStringInfo;');
+        ASCII.Add('    ManufacturerHWVersion: TCDIStringInfo;');
+        ASCII.Add('    ManufacturerSWVersion: TCDIStringInfo;');
+        ASCII.Add('  end;');
+        ASCII.Add('');
+
+        ASCII.Add('const');
+        ASCII.Add('  ' + MapIdentifier + ': TCDIStringMap = (' );
+        ASCII.Add('    (' + IntToStr(Map[0].Offset) + ', ' + IntToStr(Map[0].Length) + '),    // Manufacturer - [String Offset, Length] into the CDI Memory Space ($FF)');
+        ASCII.Add('    (' + IntToStr(Map[1].Offset) + ', ' + IntToStr(Map[1].Length) + '),    // ManufacturerModel - [String Offset, Length] into the CDI Memory Space ($FF)');
+        ASCII.Add('    (' + IntToStr(Map[2].Offset) + ', ' + IntToStr(Map[2].Length) + '),    // ManufacturerHWVersion - [String Offset, Length] into the CDI Memory Space ($FF)');
+        ASCII.Add('    (' + IntToStr(Map[3].Offset) + ', ' + IntToStr(Map[3].Length) + ')     // ManufacturerSWVersion - [String Offset, Length] into the CDI Memory Space ($FF)');
+        ASCII.Add('  );');
+
+        MemoCDI.Text := ASCII.Text;
+
+        i := Map[0].Length + Map[1].Length + Map[2].Length + Map[3].Length;
+        LabelMaxMfgStrLen.Caption := IntToStr(i);
+        if i > 64 then
+        begin
+          LabelMaxMfgStrLen.Font.Color := clRed;
+          LabelMaxMfgStrLen.Font.Style := [fsBold];
+        end else
+        begin
+          LabelMaxMfgStrLen.Font.Color := clWindowText;
+          LabelMaxMfgStrLen.Font.Style := [];
+        end;
+
+      finally
+        ASCII.Free;
+        Strings.Free
+      end;
+    finally
+      XML.Free
+    end
+  finally
+    MemoCDI.Lines.EndUpdate
+  end
+end;
+
+function TForm1.IdentifyMapItem(Delimiter, DelimiterTail, TestStr: AnsiString; MapInfo: PCDIStringInfo; ByteTotal: Integer): Boolean;
+var
+  PositionHead, PositionTail, LocalMapStartPos: Integer;
+begin
+  Result := False;
+  TestStr := LowerCase(TestStr);
+  PositionHead := Pos(Delimiter, TestStr);
+  if PositionHead > 0 then
+  begin
+    LocalMapStartPos := PositionHead + Length(Delimiter);
+    MapInfo.Offset := ByteTotal + LocalMapStartPos - 1;
+    PositionTail := PosEx(DelimiterTail, TestStr, LocalMapStartPos);
+    if PositionTail > 0 then
+    begin
+      MapInfo.Length := PositionTail - LocalMapStartPos;
+      Result := True
+    end
+  end
 end;
 
 end.
